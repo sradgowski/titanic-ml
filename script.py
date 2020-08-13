189ac05775b3cc8856d13e02ebb7c15c195a8516
Shaun Radgowski <67354145+sradgowski@users.noreply.github.com>
2020-08-13 00:39:57 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -21,9 +21,6 @@ scaler = StandardScaler()
 train_features = scaler.fit_transform(train_features)
 test_features = scaler.transform(test_features)
 
-model = LogisticRegression()
-model.fit(train_features, train_labels)
-your_age = input("What is your age? ")
 def find_gender():
     gender_input = input("What is your gender? ")
     if gender_input in ["Male", "male", "Man", "man", "Boy", "boy", "m", "M"]:
@@ -32,7 +29,7 @@ def find_gender():
         return 1.0
     else:
         print("Invalid input. Please try again.")
-your_gender = find_gender()
+        
 def find_class():
     class_input = input("""
     Which class would you have been in on the Titanic?
@@ -48,16 +45,24 @@ def find_class():
         return (0.0, 0.0)
     else:
         print("Invalid input. Please try again.")
-your_class = find_class()
-
+        
 Jack = np.array([0.0, 20.0, 0.0, 0.0])
 Rose = np.array([1.0, 17.0, 1.0, 0.0])
-You = np.array([your_gender, float(your_age), your_class[0], your_class[1]])
+        
+if __name__ == "__main__":
+    model = LogisticRegression()
+    model.fit(train_features, train_labels)
 
-new_passengers = np.array([Jack, Rose, You])
-new_passengers_transformed = scaler.transform(new_passengers)
+    your_age = input("What is your age? ")
+    your_gender = find_gender()
+    your_class = find_class()
 
-if model.predict(new_passengers_transformed)[2] == 0:
-    print("Rose survived, but unfortunately you and Jack did not.")
-if model.predict(new_passengers_transformed)[2] == 1:
-    print("Jack did not survive, but fortunately you and Rose lived!")
+    You = np.array([your_gender, float(your_age), your_class[0], your_class[1]])
+
+    new_passengers = np.array([Jack, Rose, You])
+    new_passengers_transformed = scaler.transform(new_passengers)
+
+    if model.predict(new_passengers_transformed)[2] == 0:
+        print("Rose survived, but unfortunately you and Jack did not.")
+    if model.predict(new_passengers_transformed)[2] == 1:
+        print("Jack did not survive, but fortunately you and Rose lived!")
